6f4d7ce529c27d05215acf952d57a19a792d09b3
SantiRosso <santiagorosso@live.com.ar>
2023-04-18 18:44:58 -0300

    new component: QuienesSomos

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 //styles
 import "./App.css";
 //components
-import Home from "./components/home/Home";
+import Home from "./components/views/home/Home";
+import QuienesSomos from "./components/views/quienesSomos/QuienesSomos";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/quienes-somos" element={<QuienesSomos />} />
       </Routes>
     </div>
   );
