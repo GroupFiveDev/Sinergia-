7713627721898890eda1ad090f068cdb3ac8daa6
SantiRosso <santiagorosso@live.com.ar>
2023-04-18 16:06:01 -0300

    new component: Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+//styles
 import "./App.css";
+//components
+import Home from "./components/home/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
